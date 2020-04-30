4680dd01b76d1c27ee7730d0af1ef7fcf0fe5810
k-o-s-h <nutsuki@outlook.jp>
2020-04-30 16:52:44 +0900

    can change theme

--- sakana/js/setting.js
+++ sakana/js/setting.js
@@ -2,12 +2,26 @@ const ipc = require('electron').ipcRenderer;
 
 ipc.on('path', (event, message) => {
     let config_path = message + '/config.json';
+    let theme_path = message + '/theme.json';
 
     if(check(config_path)){
-        var config_data = fs.readFileSync(config_path, 'utf8');
+        let config_data = fs.readFileSync(config_path, 'utf8');
         config_data = JSON.parse(config_data);
-        let font_size = config_data.font_size
+        let font_size = config_data.font_size;
+
         document.documentElement.style.setProperty('--fsz', font_size);
+    }
+
+    if(check(theme_path)){ 
+        let theme_data = fs.readFileSync(theme_path, 'utf8');
+        theme_data = JSON.parse(theme_data);
+        let main_c = theme_data.main;
+        let sub_c  = theme_data.sub;
+        let text_c = theme_data.text;
+
+        document.documentElement.style.setProperty('--bgc',  main_c);
+        document.documentElement.style.setProperty('--sttc', sub_c );
+        document.documentElement.style.setProperty('--ttc',  text_c);
     }
 });
 
